5eb57bdce5fd21de2d3d55d0655955ff1e118edf
shahbaznew <shahbazayub0@gmail.com>
2024-09-25 22:55:31 +0100

    creating inital validation and import it into the correct file for a schema

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,24 +1,22 @@
-import { z } from "zod"
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
-
+import { SignupValidation } from "@/lib/validation"
+import { z } from "zod";
+ 
 const SignupForm = () => {
 
- const form = useForm<z.infer<typeof formSchema>>({
-  resolver: zodResolver(formSchema),
+ const form = useForm<z.infer<typeof SignupValidation>>({
+  resolver: zodResolver(SignupValidation),
   defaultValues: {
     username: "",
   },
 })
 
-function onSubmit(values: z.infer<typeof formSchema>) {
+function onSubmit(values: z.infer<typeof SignupValidation>) {
   console.log(values)
 }
 
