f46578105818af96678b3e88821239d38aab392f
shahbaznew <shahbazayub0@gmail.com>
2024-09-25 01:41:23 +0100

    Routes created and connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import {Routes, Route, Router } from 'react-router-dom'; 
-
-
 import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
-import Home from './_root/pages/Home';
+import Home  from './_root/pages/Home';
 import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 
 const App = () => {
@@ -12,13 +12,16 @@ const App = () => {
     <main className='flex h-screen'>
         <Routes> 
             {/*public routes*/}
+            <Route element={<AuthLayout /> }> 
             <Route path="/sign-in" element={< SigninForm />} />
             <Route path="/sign-up" element={< SignupForm />} />
-
+            </Route>
 
 
             {/*private routes*/}
+            <Route element={<RootLayout /> }> 
             <Route index element={<Home />}/>
+            </Route>
         </Routes>
     </main>
   )
